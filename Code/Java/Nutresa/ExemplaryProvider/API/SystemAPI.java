d500106a99a975a270b27b86eac4ac1f1f096324
Diego Romero <diegoromero1014@outlook.com>
2017-10-30 15:28:08 -0500

    Mob programming de System

--- Code/Java/Nutresa/ExemplaryProvider/API/SystemAPI.java
+++ Code/Java/Nutresa/ExemplaryProvider/API/SystemAPI.java
@@ -3,49 +3,12 @@ package Nutresa.ExemplaryProvider.API;
 import Nutresa.ExemplaryProvider.BLL.SystemBLO;
 import Nutresa.ExemplaryProvider.DTL.SystemDTO;
 
-public class SystemAPI {
-
-    private SystemDTO dto;
-
-    public SystemAPI() {
-	dto = new SystemDTO();
+public class SystemAPI extends BaseAPI {
+    private SystemDTO system;
+    
+    public SystemDTO getSystem(String idSystem){
+        SystemBLO systemBLO = new SystemBLO();
+        return systemBLO.getSystem(idSystem);
     }
-
-    public SystemDTO getDto() {
-	return dto;
-    }
-
-    public String getDtoJson() {
-	// GsonBuilder builder = new GsonBuilder();
-	// Gson gson = builder.create();
-	// System.out.println("Antes");
-	// String json = gson.toJson(this.dto);
-	return this.dto.getJson();
-	// String json = "";
-	// try {
-	// json = JsonGenerator.toJson(JsonJavaFactory.instance, this.dto);
-	// } catch (JsonException e) {
-	// // TODO Auto-generated catch block
-	// e.printStackTrace();
-	// } catch (IOException e) {
-	// // TODO Auto-generated catch block
-	// e.printStackTrace();
-	// }
-	// System.out.println(json);
-	// return json;
-    }
-
-    public void setDto(SystemDTO dto) {
-	this.dto = dto;
-    }
-
-    public void load() {
-	SystemBLO systemBLO = new SystemBLO();
-	dto = systemBLO.loadSystem();
-    }
-
-    public void save() {
-	SystemBLO systemBLO = new SystemBLO();
-	systemBLO.saveSystem(dto);
-    }
+    
 }
--- Code/Java/Nutresa/ExemplaryProvider/DTL/SystemDTO.java
+++ Code/Java/Nutresa/ExemplaryProvider/DTL/SystemDTO.java
@@ -1,55 +1,5 @@
 package Nutresa.ExemplaryProvider.DTL;
 
-import java.util.Date;
-
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.annotations.Expose;
-
 public class SystemDTO {
-
-	@Expose
-	private String id;
-	private String host = "";
-	private String sharedFolder = "5";
-	private String form = "frSystem";
-	private Date date;
-	
-	public String getJson() {
-		 date = new Date();
-		 Gson gson = new GsonBuilder()
-	     .enableComplexMapKeySerialization()
-	     .excludeFieldsWithoutExposeAnnotation()
-	     .serializeNulls()
-	     .setDateFormat("Y/m/d")
-	     .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-	     .setPrettyPrinting()
-	     .setVersion(1.0)
-	     .create();
-		System.out.println("Antes");
-		return gson.toJson(this);
-	}
-
-//	public String getForm() {
-//		return form;
-//	}
-//	public String getId() {
-//		return id;
-//	}
-//	public void setId(String id) {
-//		this.id = id;
-//	}
-//	public String getHost() {
-//		return host;
-//	}
-//	public void setHost(String host) {
-//		this.host = host;
-//	}
-//	public String getSharedFolder() {
-//		return sharedFolder;
-//	}
-//	public void setSharedFolder(String sharedFolder) {
-//		this.sharedFolder = sharedFolder;
-//	}
+	private String host;
 }
