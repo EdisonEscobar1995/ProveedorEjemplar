f2174fbd6b849634d572896e039cc47b09145192
Juan Carlos González <jcgonzalez@cognox.com>
2017-11-09 14:57:01 -0500

    AlertDAO y CallDAO

---
+++ Code/Java/com/nutresa/exemplary_provider/dal/AlertDAO.java
@@ -0,0 +1,11 @@
+package com.nutresa.exemplary_provider.dal;
+
+import com.nutresa.exemplary_provider.dtl.AlertDTO;
+
+public class AlertDAO extends GenericDAO<AlertDTO>{
+   
+	public AlertDAO() {
+        super(AlertDTO.class);
+    }
+	
+}
