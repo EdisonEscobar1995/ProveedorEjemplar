f56e875deec2df4d61759b9251af7651664ebdd3
Sergio Escudero <sergioescudero@sergios-mbp.cognox.com>
2017-11-18 15:43:18 -0500

    GENERAL Dinamic deploy per server

--- frontend/src/api/instance.js
+++ frontend/src/api/instance.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const env = process.env.REACT_APP_ENV;
+let baseUrl = process.env.REACT_APP_URL_API;
+if (env !== 'DEV') {
+  const deployFolder = process.env.REACT_APP_DEPLOY_FOLDER;
+  baseUrl = `${baseUrl.split(deployFolder)[0]}xsp`;
+}
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_URL_API,
+  baseURL: baseUrl,
 });
 
 export {
