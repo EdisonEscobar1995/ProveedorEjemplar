bedddd5305c2e5ae8da536ff050e4892117583d9
Diego Romero <daromero@cognox.com>
2017-11-19 14:09:42 -0500

    HU-2 Administrar Convocatorias Convocatoria por proveedor Obtener las convocatorias que tienen un proveedor, prevaleciendo la que este vigente según su fecha de cierre.

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/SupplierByCallBLO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/SupplierByCallBLO.java
@@ -1,9 +1,12 @@
 package com.nutresa.exemplary_provider.bll;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import com.nutresa.exemplary_provider.dal.CallDAO;
 import com.nutresa.exemplary_provider.dal.SupplierByCallDAO;
+import com.nutresa.exemplary_provider.dtl.CallDTO;
 import com.nutresa.exemplary_provider.dtl.SupplierByCallDTO;
 import com.nutresa.exemplary_provider.utils.HandlerGenericException;
 
@@ -12,16 +15,28 @@ public class SupplierByCallBLO extends GenericBLO<SupplierByCallDTO, SupplierByC
         super(SupplierByCallDAO.class);
     }
 
-    public List<SupplierByCallDTO> getCurrentCallBySupplier() throws HandlerGenericException {
+    public SupplierByCallDTO getCurrentCallBySupplier() throws HandlerGenericException {
         SupplierByCallDAO supplierByCallDAO = new SupplierByCallDAO();
+        CallDAO callDAO = new CallDAO();
+        CallDTO call = null;
+        SupplierByCallDTO response = null;
         SupplierBLO supplierBLO = new SupplierBLO();
         List<SupplierByCallDTO> callsBySupplier = new ArrayList<SupplierByCallDTO>();
         try {
             callsBySupplier = supplierByCallDAO.getBySupplier(supplierBLO.getSupplierInSession().getId());
+            for (SupplierByCallDTO supplierByCall : callsBySupplier) {
+                call = callDAO.get(supplierByCall.getIdCall());
+                if (call.isNotCaduced(new Date())) {
+                    response = supplierByCall;
+                    break;
+                } else {
+                    response = null;
+                }
+            }
         } catch (HandlerGenericException exception) {
             throw new HandlerGenericException(exception);
         }
 
-        return callsBySupplier;
+        return response;
     }
 }
