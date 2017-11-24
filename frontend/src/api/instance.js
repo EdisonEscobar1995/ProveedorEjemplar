0da8e4da9b946cba060d5ac446d6503f5ceef035
Sergio Escudero <sergioescudero@sergios-mbp.cognox.com>
2017-11-24 08:07:12 -0500

    GENERAL Url dinamica para servicios

--- frontend/src/api/instance.js
+++ frontend/src/api/instance.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-const env = process.env.REACT_APP_ENV;
+const env = process.NODE_ENV;
 let baseUrl = process.env.REACT_APP_URL_API;
-if (env !== 'DEV') {
+if (env !== 'development') {
   const deployFolder = process.env.REACT_APP_DEPLOY_FOLDER;
   baseUrl = `${baseUrl.split(deployFolder)[0]}xsp`;
 }
 const instance = axios.create({
   baseURL: baseUrl,
-  headers: { 'Content-type': 'application/json; charset=utf-8' },
+  headers: { 'Content-type': 'application/json' },
 });
 
 export {
