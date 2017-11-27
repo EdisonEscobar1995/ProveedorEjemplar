7a8be46ef5facb44e9525e442e7c568a625dc721
Diego Romero <diegoromero1014@outlook.com>
2017-11-27 12:29:50 -0500

    HU-4	Participar del Programa Respuestas Eliminar las respuestas cuando intente cambiar el tamaño de la empresa

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/api/AnswerAPI.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/api/AnswerAPI.java
@@ -2,25 +2,11 @@ package com.nutresa.exemplary_provider.api;
 
 import com.nutresa.exemplary_provider.bll.AnswerBLO;
 import com.nutresa.exemplary_provider.dtl.AnswerDTO;
-import com.nutresa.exemplary_provider.dtl.ServletResponseDTO;
-import com.nutresa.exemplary_provider.utils.HandlerGenericException;
 
 public class AnswerAPI extends GenericAPI<AnswerDTO, AnswerBLO> {
 
     public AnswerAPI() {
         super(AnswerDTO.class, AnswerBLO.class);
-    }
-
-    public ServletResponseDTO<AnswerDTO> deleteAnswers(AnswerDTO answer) {
-        AnswerBLO answerBLO = new AnswerBLO();
-        ServletResponseDTO<AnswerDTO> response = null;
-        try {
-            response = new ServletResponseDTO<AnswerDTO>(answerBLO.deleteAnswers(answer));
-        } catch (HandlerGenericException exception) {
-            response = new ServletResponseDTO<AnswerDTO>(exception);
-        }
-
-        return response;
     }
 
 }
--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/AnswerBLO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/AnswerBLO.java
@@ -1,7 +1,6 @@
 package com.nutresa.exemplary_provider.bll;
 
 import com.nutresa.exemplary_provider.dal.AnswerDAO;
-import com.nutresa.exemplary_provider.dtl.AnswerDTO;
 import com.nutresa.exemplary_provider.dtl.OptionDTO;
 import com.nutresa.exemplary_provider.utils.HandlerGenericException;
 
@@ -11,10 +10,9 @@ public class AnswerBLO extends GenericBLO<OptionDTO, AnswerDAO> {
         super(AnswerDAO.class);
     }
 
-    public AnswerDTO deleteAnswers(AnswerDTO answer) throws HandlerGenericException {
+    public void deleteAnswers(String idSupplierByCall) throws HandlerGenericException {
         AnswerDAO answerDAO = new AnswerDAO();
-        answerDAO.deleteBySupplier(answer.getIdSupplierByCall());
-        return null;
+        answerDAO.deleteBySupplier(idSupplierByCall);
     }
 
 }
