a3e8fa5339602eac6a60b673be1eb4753e0c24c8
Diego Romero <diegoromero1014@outlook.com>
2017-11-27 12:21:21 -0500

    HU-4	Participar del Programa Respuestas Eliminar las respuestas cuando intente cambiar el tamaño de la empresa

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/AnswerBLO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/AnswerBLO.java
@@ -1,12 +1,20 @@
 package com.nutresa.exemplary_provider.bll;
 
 import com.nutresa.exemplary_provider.dal.AnswerDAO;
+import com.nutresa.exemplary_provider.dtl.AnswerDTO;
 import com.nutresa.exemplary_provider.dtl.OptionDTO;
+import com.nutresa.exemplary_provider.utils.HandlerGenericException;
 
 public class AnswerBLO extends GenericBLO<OptionDTO, AnswerDAO> {
 
     public AnswerBLO() {
         super(AnswerDAO.class);
+    }
+
+    public AnswerDTO deleteAnswers(AnswerDTO answer) throws HandlerGenericException {
+        AnswerDAO answerDAO = new AnswerDAO();
+        answerDAO.deleteBySupplier(answer.getIdSupplierByCall());
+        return null;
     }
 
 }
