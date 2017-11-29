bcefb381c0e9e91d0833d2cd3173271d584c025c
Sergio Escudero <sergioescudero@sergios-mbp.cognox.com>
2017-11-29 00:33:36 -0500

    GENERAL Estilos de la app

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -4,12 +4,15 @@ const theme = {
     secondary: '#7fba00',
     info: '#989898',
     error: '#f04134',
+    normal: '#FFF',
+    back: '#f0f0f0',
   },
   spaces: {
     main: '15px',
   },
   images: {
     logo: 'assets/images/LogoApp.png',
+    surveyIcon: 'assets/images/SurveyIcon.png',
   },
 };
 export default theme;
