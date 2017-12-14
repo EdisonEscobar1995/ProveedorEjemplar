717758a6e3029f89d4f1ad9a492a952644f6f14d
Diego Romero <daromero@cognox.com>
2017-12-14 11:50:10 -0500

    HU-4	Participar del Programa Respuesta Eliminar respuestas de forma masiva.

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/api/AnswerAPI.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/api/AnswerAPI.java
@@ -2,11 +2,25 @@ package com.nutresa.exemplary_provider.api;
 
 import com.nutresa.exemplary_provider.bll.AnswerBLO;
 import com.nutresa.exemplary_provider.dtl.AnswerDTO;
+import com.nutresa.exemplary_provider.dtl.ServletResponseDTO;
+import com.nutresa.exemplary_provider.utils.HandlerGenericException;
 
 public class AnswerAPI extends GenericAPI<AnswerDTO, AnswerBLO> {
 
     public AnswerAPI() {
         super(AnswerDTO.class, AnswerBLO.class);
+    }
+    
+    public ServletResponseDTO<AnswerDTO> deleteMassive(AnswerDTO answer){
+        ServletResponseDTO<AnswerDTO> response  = null;
+        AnswerBLO answerBLO = new AnswerBLO();
+        try {
+            response = new ServletResponseDTO<AnswerDTO>(answerBLO.deleteMassive(answer));
+        } catch (HandlerGenericException exception) {
+            response = new ServletResponseDTO<AnswerDTO>(exception);
+        }
+        
+        return response;
     }
 
 }
