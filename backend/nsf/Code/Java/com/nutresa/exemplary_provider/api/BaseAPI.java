1ae8e8e9af4311f64a119d9c64ca4a31de329d91
Diego Romero <diegoromero1014@outlook.com>
2018-03-05 14:40:41 -0500

    HU-6 - HU-45 Maestros y listado de registros

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/api/BaseAPI.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/api/BaseAPI.java
@@ -95,7 +95,6 @@ public class BaseAPI<T> extends DesignerFacesServlet {
             parameters.remove("action");
 
             List<String> access = getACL();
-            access.add("*.*");
 
             if (requestMethod != TypeRequestMethod.OPTIONS
                     && !validateAccess(access, this.getClass().getSimpleName(), action)) {
