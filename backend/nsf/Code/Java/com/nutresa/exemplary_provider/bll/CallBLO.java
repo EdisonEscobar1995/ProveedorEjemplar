9230b668a9558eabe99731f1d206728c7c669795
Diego Romero <daromero@cognox.com>
2018-04-02 17:09:29 -0500

    HU-48 Respuestas por miembro del comité - Funcionalidad para devolver las respuestas dadas por el miembro del   comité gerencial que esté en sesión.

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/CallBLO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/CallBLO.java
@@ -356,7 +356,7 @@ public class CallBLO extends GenericBLO<CallDTO, CallDAO> {
                 filter.put("whoEvaluate", nameUserInSession);
             }
             List<DTO> auxiliarAnswer = managerTeamAnswerBLO.getAllBy(filter,
-                    "vwManagerTeamAnswersByIdSupplierByCallAndIdUser");
+                    "vwManagerTeamAnswersByIdSupplierByCallAndWhoEvaluate");
 
             if (!auxiliarAnswer.isEmpty()) {
                 answers.addAll(auxiliarAnswer);
