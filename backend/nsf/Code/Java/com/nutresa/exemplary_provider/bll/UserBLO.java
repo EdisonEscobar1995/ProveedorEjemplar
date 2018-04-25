bca2e4f8fc29bed6e684f8d0787f0163665c2dc9
Diego Romero <daromero@cognox.com>
2018-04-25 17:02:10 -0500

    HU-1 ADMINISTRACIÓN GENERAL Información del proveedor - Al iniciar sesión, se devuelve información adicional del provedor, el   cual será utilizada para ingresar a la encuesta desde el HOME

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/UserBLO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/UserBLO.java
@@ -17,6 +17,7 @@ import com.nutresa.exemplary_provider.dtl.HandlerGenericExceptionTypes;
 import com.nutresa.exemplary_provider.dtl.Rol;
 import com.nutresa.exemplary_provider.dtl.RolDTO;
 import com.nutresa.exemplary_provider.dtl.SupplierByCallDTO;
+import com.nutresa.exemplary_provider.dtl.SupplierDTO;
 import com.nutresa.exemplary_provider.dtl.TechnicalTeamDTO;
 import com.nutresa.exemplary_provider.dtl.UserDTO;
 import com.nutresa.exemplary_provider.utils.Common;
@@ -78,6 +79,16 @@ public class UserBLO extends GenericBLO<UserDTO, UserDAO> {
             Name dominoUser = userDAO.getDominoUser();
             Map<String, String> userInfo = new LinkedHashMap<String, String>();
             userInfo.put("name", dominoUser.getNamePart(NamePartKey.Common));
+
+            SupplierBLO supplierBLO = new SupplierBLO();
+            SupplierDTO supplier = supplierBLO.getSupplierInSession(null);
+            if (null != supplier) {
+                CompanySizeBLO companySizeBLO = new CompanySizeBLO();
+                userInfo.put("company_size", companySizeBLO.get(supplier.getIdCompanySize()).getName());
+                SupplyBLO supplyBLO = new SupplyBLO();
+                userInfo.put("supply", supplyBLO.get(supplier.getIdSupply()).getName());
+            }
+
             userInfo.put("canonical", dominoUser.getNamePart(NamePartKey.Canonical));
             userContext.put("userInfo", userInfo);
             userContext.put("rols", rols);
