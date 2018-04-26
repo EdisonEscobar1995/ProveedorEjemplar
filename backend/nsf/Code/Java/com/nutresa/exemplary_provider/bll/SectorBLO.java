b5a02cd770d1e9586d13393e20911e014faa5163
Diego Romero <daromero@cognox.com>
2018-04-26 14:31:57 -0500

    HU-12  ADMINISTRAR SECTORES Validar al guardar - Al momento de guardar se valida que no exista un sector con el mismo   nombre

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/SectorBLO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/bll/SectorBLO.java
@@ -1,12 +1,66 @@
 package com.nutresa.exemplary_provider.bll;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.nutresa.exemplary_provider.dal.SectorDAO;
+import com.nutresa.exemplary_provider.dtl.HandlerGenericExceptionTypes;
 import com.nutresa.exemplary_provider.dtl.SectorDTO;
+import com.nutresa.exemplary_provider.utils.HandlerGenericException;
 
 public class SectorBLO extends GenericBLO<SectorDTO, SectorDAO> {
 
     public SectorBLO() {
         super(SectorDAO.class);
+    }
+
+    @Override
+    public SectorDTO save(SectorDTO sector) throws HandlerGenericException {
+        SectorDTO response = null;
+
+        if (null != sector.getName()) {
+            if (existSector(sector)) {
+                throw new HandlerGenericException(HandlerGenericExceptionTypes.DOCUMENT_EXISTS.toString());
+            } else {
+                response = super.save(sector);
+            }
+        } else {
+            throw new HandlerGenericException(HandlerGenericExceptionTypes.UNEXPECTED_VALUE.toString());
+        }
+
+        return response;
+    }
+
+    private boolean existSector(SectorDTO sector) throws HandlerGenericException {
+        boolean existItem = false;
+
+        List<String> filter = new ArrayList<String>();
+        filter.add(sector.getName());
+
+        String idItem = sector.getId();
+        String temporalIdentifier = sector.getName().trim();
+
+        SectorDAO sectorDAO = new SectorDAO();
+        List<SectorDTO> existingSectors = sectorDAO.getByProperties(filter);
+
+        if (!existingSectors.isEmpty()) {
+            SectorDTO existingSector = existingSectors.get(0);
+
+            String idItemExisting = existingSector.getId();
+            String temporalIdentifierExisting = existingSector.getName().trim();
+
+            if ((null == idItem || idItem.isEmpty()) && (null != temporalIdentifierExisting
+                    && temporalIdentifier.equalsIgnoreCase(temporalIdentifierExisting))) {
+                existItem = true;
+            } else {
+                if (null != idItem && null != idItemExisting && !idItem.equals(idItemExisting)
+                        && temporalIdentifier.equalsIgnoreCase(temporalIdentifierExisting)) {
+                    existItem = true;
+                }
+            }
+        }
+
+        return existItem;
     }
 
 }
--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/dal/SectorDAO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/dal/SectorDAO.java
@@ -1,12 +1,33 @@
 package com.nutresa.exemplary_provider.dal;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openntf.domino.Document;
+import org.openntf.domino.DocumentCollection;
+import org.openntf.domino.View;
+
 import com.nutresa.exemplary_provider.dtl.SectorDTO;
+import com.nutresa.exemplary_provider.utils.HandlerGenericException;
 
 public class SectorDAO extends GenericDAO<SectorDTO> {
 
     public SectorDAO() {
         super(SectorDTO.class);
         loadTranslator();
+    }
+
+    public List<SectorDTO> getByProperties(List<String> filter) throws HandlerGenericException {
+        List<SectorDTO> sector = new ArrayList<SectorDTO>();
+        View currentView = getDatabase().getView("vwSectorByName");
+        DocumentCollection documents = currentView.getAllDocumentsByKey(filter, true);
+        if (null != documents) {
+            for (Document document : documents) {
+                sector.add(castDocument(document));
+            }
+        }
+
+        return sector;
     }
 
 }
