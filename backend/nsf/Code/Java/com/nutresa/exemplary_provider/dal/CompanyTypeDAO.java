dbe89187e11d369b6985524c825c0450b0861579
Diego Romero <daromero@cognox.com>
2018-04-26 13:03:52 -0500

    HU-5  ADMINISTRAR TIPOS DE COMPAÑÍA Validaciones - Se validó que no se guardara un registro si ya existía uno con las   mismas propiedades. - Se agregó validación para no dejar eliminar documentos que ya estén   siendo utilizados por otros.

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/dal/CompanyTypeDAO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/dal/CompanyTypeDAO.java
@@ -1,12 +1,33 @@
 package com.nutresa.exemplary_provider.dal;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openntf.domino.Document;
+import org.openntf.domino.DocumentCollection;
+import org.openntf.domino.View;
+
 import com.nutresa.exemplary_provider.dtl.CompanyTypeDTO;
+import com.nutresa.exemplary_provider.utils.HandlerGenericException;
 
 public class CompanyTypeDAO extends GenericDAO<CompanyTypeDTO> {
 
     public CompanyTypeDAO() {
         super(CompanyTypeDTO.class);
         loadTranslator();
+    }
+
+    public List<CompanyTypeDTO> getByProperties(List<String> filter) throws HandlerGenericException {
+        List<CompanyTypeDTO> scales = new ArrayList<CompanyTypeDTO>();
+        View currentView = getDatabase().getView("vwCompanyTypeByName");
+        DocumentCollection documents = currentView.getAllDocumentsByKey(filter, true);
+        if (null != documents) {
+            for (Document document : documents) {
+                scales.add(castDocument(document));
+            }
+        }
+
+        return scales;
     }
 
 }
