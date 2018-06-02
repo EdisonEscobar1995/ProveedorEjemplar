c15dbbaf1add856ca50b50d0847fedf529518e75
Diego Romero <daromero@cognox.com>
2018-06-01 21:54:39 -0500

    GENERAL Corrección de errores
    I

--- backend/nsf/Code/Java/com/nutresa/exemplary_provider/dtl/SupplierDTO.java
+++ backend/nsf/Code/Java/com/nutresa/exemplary_provider/dtl/SupplierDTO.java
@@ -380,6 +380,10 @@ public class SupplierDTO {
     public String getId() {
         return id;
     }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getIdCompanySize() {
         return idCompanySize;
